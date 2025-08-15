58cfc2edced8e7a27c9698842ce42afef5aad7cc
amayavijayan21 <amayavijayan937@gmail.com>
2025-08-15 11:37:45 +0530

    refactor: clean up main.dart by removing commented-out code

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,3 @@
-// import 'package:flutter/material.dart';
-
-
-// void main() {
-//   runApp(const E_Commerce());
-// }
-
-// class E_Commerce extends StatelessWidget {
-//   const E_Commerce({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       // home: Home(),
-//     );
-//   }
-// }
-
-
-
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
